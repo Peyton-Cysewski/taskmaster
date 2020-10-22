f8a84017bc7e26314e1b20cae3c6af9412443f77
Peyton Cysewski <pcysewski12@gmail.com>
2020-10-22 00:45:21 -0700

    updated readme and completed lab 26

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# taskmaster
+# Task Master
+
+## Description
+This is an android application that allows the user to add and keep track of current tasks that they may have.
+
+### Changelog
+ - 1.0 *Setup and Release* - 22 October 2020<br>
+ &#9 A static landing page allows the user to navigate to a page to add new tasks or to a page to view all current tasks. Neither page has any true functionality, but acts as more of a mock up at this point.
--- app/src/main/java/com/peytoncysewski/taskmaster/AddTask.java
+++ app/src/main/java/com/peytoncysewski/taskmaster/AddTask.java
@@ -1,8 +1,13 @@
 package com.peytoncysewski.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class AddTask extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class AddTask extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+//        trying to add a Snackbar pop up message
+//        Button addTaskButton = AddTask.this.findViewById(R.id.add_task_button);
+//        addTaskButton.setOnClickListener(view -> {
+//            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), "Task Added", BaseTransientBottomBar.LENGTH_SHORT);
+//            snackbar.show();
+//        });
     }
 }
