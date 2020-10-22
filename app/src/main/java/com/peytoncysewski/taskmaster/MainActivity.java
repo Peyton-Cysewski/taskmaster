8c18156258d9a4dbce047deb5bd19b9e770130d7
Peyton Cysewski <pcysewski12@gmail.com>
2020-10-22 00:13:28 -0700

    added activities

--- app/src/main/java/com/peytoncysewski/taskmaster/MainActivity.java
+++ app/src/main/java/com/peytoncysewski/taskmaster/MainActivity.java
@@ -2,7 +2,9 @@ package com.peytoncysewski.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button allTasksButton = MainActivity.this.findViewById(R.id.goto_all_tasks_button);
+        allTasksButton.setOnClickListener(view -> {
+            Intent goToAllTasks = new Intent(MainActivity.this, AllTasks.class);
+            MainActivity.this.startActivity(goToAllTasks);
+        });
+
+        Button addTaskButton = MainActivity.this.findViewById(R.id.goto_add_task_button);
+        addTaskButton.setOnClickListener(view -> {
+            Intent goToAddTask = new Intent(MainActivity.this, AddTask.class);
+            MainActivity.this.startActivity(goToAddTask);
+        });
     }
 }
