3ace048d87424995dae5697877b899ebcb7c89d5
Peyton Cysewski <pcysewski12@gmail.com>
2020-10-25 11:41:19 -0700

    added screenshot to readme and added toast notification on the Add Task page

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This is an android application that allows the user to add and keep track of cur
 ### Changelog
  - 1.0 *Setup and Release* - 22 October 2020<br>
  &#9 A static landing page allows the user to navigate to a page to add new tasks or to a page to view all current tasks. Neither page has any true functionality, but acts as more of a mock up at this point.
+ ![Home Page](./screenshots/home-page-lab-26.png)
--- app/src/main/java/com/peytoncysewski/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/peytoncysewski/taskmaster/AddTaskActivity.java
@@ -3,24 +3,27 @@ package com.peytoncysewski.taskmaster;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
-public class AddTask extends AppCompatActivity {
+public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
 
-//        trying to add a Snackbar pop up message
-//        Button addTaskButton = AddTask.this.findViewById(R.id.add_task_button);
-//        addTaskButton.setOnClickListener(view -> {
-//            Snackbar snackbar = Snackbar.make(findViewById(R.id.parent), "Task Added", BaseTransientBottomBar.LENGTH_SHORT);
-//            snackbar.show();
-//        });
+        Button addTaskButton = AddTaskActivity.this.findViewById(R.id.add_task_button);
+        addTaskButton.setOnClickListener(view -> {
+            Context context = getApplicationContext();
+            String text = "Task Added!";
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+        });
     }
 }
